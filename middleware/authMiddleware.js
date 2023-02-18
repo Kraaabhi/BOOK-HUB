09e6f847eac5d2553bac4e565b1ba872fca49fe4
Abhishek Kumar <kraabhi2001@gmail.com>
2023-02-19 02:13:21 +0530

    cleaning code for middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,12 @@
 function authMiddleware(req, res, next) {
-    const apiKey = req.headers['x-api-key'];
-    const API_KEY=process.env.API_KEY
+  const apiKey = req.headers['x-api-key']
+  const API_KEY = process.env.API_KEY
 
-  
-    if (apiKey === API_KEY) {
-      next();
-    } else {
-      res.status(401).json({ error: 'Unauthorized' });
-    }
+  if (apiKey === API_KEY) {
+    next()
+  } else {
+    res.status(401).json({ error: 'Unauthorized' })
   }
-  
-  module.exports = authMiddleware;
+}
+
+module.exports = authMiddleware
--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,8 +1,12 @@
-const winston = require('winston');
+const winston = require('winston')
 
 const handleError = (err, req, res, next) => {
-  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-  res.status(err.status || 500).json({ error: err.message });
-};
+  winston.error(
+    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
+      req.method
+    } - ${req.ip}`,
+  )
+  res.status(err.status || 500).json({ error: err.message })
+}
 
-module.exports = { handleError };
+module.exports = { handleError }
